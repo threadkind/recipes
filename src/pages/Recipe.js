4e64b2167631e73934a1fe944f84820ffeaae163
threadkind <threadkind@gmail.com>
2020-08-10 02:09:15 -0500

    Add modfrom array

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -19,26 +19,63 @@ class Recipe extends React.Component {
       <h3>{data[this.state.recipeId].title}</h3>
       <p>Makes {data[this.state.recipeId].servings}</p>
 
-      {data[this.state.recipeId].modFrom !== '' &&
+      {data[this.state.recipeId].modFrom !== [''] &&
         <p>
-          Modified from <a href={data[this.state.recipeId].modFrom} target="_blank">here</a>.
+          Modified from
+
+          {data[this.state.recipeId].modFrom.map((m, index) => {
+            if(index == data[this.state.recipeId].modFrom.length-1 && data[this.state.recipeId].modFrom.length > 1){
+             return(
+                <a href={m} target="_blank"> and here</a>
+              )
+            }
+            else{
+              return(
+                <a href={m} target="_blank"> here</a>
+              )
+            }
+          })
+          }
         </p>
       }
       <img className="recipeMainImage" src={data[this.state.recipeId].photo} alt={data[this.state.recipeId].title} />
 
       <h2>INGREDIENTS</h2>
       <ul className="recipeIngredients">
-      {data[this.state.recipeId].ingredients.map((x, index) =>
-        <li key={index}>{x}</li>
+      {data[this.state.recipeId].ingredients.map((x, index) => {
+        if(x.toUpperCase() == x ){
+          return(
+
+            <li key={index} className="strong"><br/>{x}</li>
+          )
+        }
+        else {
+          return (
+            <li key={index}>{x}</li>
+          )
+
+        }
+
+
+      }
       )}
       </ul>
       <br />
       <h2>DIRECTIONS</h2>
       <ul className="recipeDirections">
-      {data[this.state.recipeId].directions.map((x, index) =>
+      {data[this.state.recipeId].directions.map((x, index) => {
+        if(x.toUpperCase() == x ){
+          return(
 
-        <li key={index}>{x}</li>
-
+            <li key={index} className="strong"><br/>{x}</li>
+          )
+        }
+        else {
+          return (
+            <li key={index}>{x}</li>
+          )
+        }
+      }
       )}
       <br />
       <li>Enjoy!</li>
